ce281c4cdf83359d445fcab9aaeedd097a466e64
tcox <taylorcox75@gmail.com>
2016-01-23 05:28:39 -0700

    Some changes to the file to allow a standby function

--- keyStroke.js
+++ keyStroke.js
@@ -7,6 +7,7 @@ var keypress = require('keypress');
 var client = arDrone.createClient();
 client.createRepl();
 keypress(process.stdin);
+client._ref.fly = false;
 
 /**
  * These lines of code look for a keypress on stdin.  When one is recieved, it is determined
@@ -14,88 +15,53 @@ keypress(process.stdin);
  */
 process.stdin.on('keypress', function (ch, key) {
     //console.log('got "keypress"', key);
-    if (key.name === 't') {
-        console.log('\nClient Took Off')//\nEnter a Command:')
-        client.takeoff()
-    } else if (key.name === 'space') {
-        console.log('\nClient Landed')//\nEnter a Command:')
+    if (key.name === 'space' && client._ref.fly) {
+        console.log('Client Landed')
         client.land()
+    } else if (key.name === 'space') {
+        console.log('Spinning Up!')
+        client.takeoff()
     } else if (key.name === 'w') {
-        console.log('\nGoing Up!')//\nEnter a Command:')
+        console.log('\nGoing Up!')
         client.up(1)
     } else if (key.name === 's') {
-        console.log('\nGoing Down!')//\nEnter a Command:')
+        console.log('Going Down!')
         client.down(1)
     } else if (key.name === 'a') {
-        console.log('\nRotating CCW')//\nEnter a Command:')
+        console.log('Rotating CCW')
         client.counterClockwise(1)
     } else if (key.name === 'd') {
-        console.log('\nRotating CW')//\nEnter a Command:')
+        console.log('Rotating CW')
         client.clockwise(1)
     } else if (key.name === 'up') {
-        console.log('\nGoing Forward')//\nEnter a Command:')
+        console.log('Going Forward')
         client.front(1)
     } else if (key.name === 'down') {
-        console.log('\nGoing Backwards')//\nEnter a Command:')
+        console.log('Going Backwards')
         client.back(1)
     } else if (key.name === 'left') {
-        console.log('\nTo The Left <-')//\nEnter a Command:')
+        console.log('To The Left <-')
         client.left(1)
     } else if (key.name === 'right') {
-        console.log('\nTo The Right ->')//\nEnter a Command:')
+        console.log('To The Right ->')
         client.right(1)
     }
-    // This timesout the drone wanting to keep moving after the button is released
+    else if (key && key.ctrl && key.name == 'c') {
+        console.log('EMERGENCY LANDING')
+        client.land();
+
+    }
+
+     //This timesout the drone wanting to keep moving after the button is released
     setTimeout(function () {
         client.stop();
-        console.log("\nEnter a Command:")
+        console.log("drone>")
     }, 1250);
 
+
 });
-function takeOff() {
-    console.log("\ntakeOff");
-    client.takeoff();
-}
-function counterClockwise() {
-    console.log("\ncounterClockwise");
 
-}
-function clockwise() {
-    console.log("\nclockwise");
-    client.clockwise(0.75);
-}
-function altUp() {
-    console.log("\naltUp");
-    client.up(0.75);
-}
-function altDown() {
-    console.log("\naltDown");
-    client.down(0.75);
-}
-function space() {
-    console.log("\nland");
-    client.land();
-}
-function up() {
-    console.log("\nforward");
-    client.front(1)
-}
-function left() {
-    console.log("\nleft");
-    client.left(0.75)
-}
-function down() {
-    console.log("\ndown");
-    client.back(1)
-}
-function right() {
-    console.log("\nright");
-    client.right(0.75)
-
-}
-function nothing() {
-    console.log('nothing')
-}
 
 process.stdin.setRawMode(true);
 process.stdin.resume();
+
