445db83dc190d75584ed2c632e99ec0f22129751
tcox <taylorcox75@gmail.com>
2016-01-23 02:00:42 -0700

    Implements a server on localhost:5555 where the video can be streamed

--- keyStroke.js
+++ keyStroke.js
@@ -4,8 +4,9 @@ var keypress = require('keypress');
 keypress(process.stdin);
 
 // listen for the "keypress" event
+
 process.stdin.on('keypress', function (ch, key) {
-  console.log('got "keypress"', key);
+  //console.log('got "keypress"', key);
   if(key.name === 'up'){
   	up();
   }else if(key.name === 'left'){
@@ -14,11 +15,14 @@ process.stdin.on('keypress', function (ch, key) {
   	down();
   }else if(key.name === 'right'){
   	right();
+  }else if(key.name === null){
+      nothing();
   }
   if (key && key.ctrl && key.name == 'c') {
     process.stdin.pause();
   }
 });
+
 
 function up(){
 	console.log("forward");
@@ -31,6 +35,9 @@ function down(){
 }
 function right(){
 	console.log("clockwise");
+}
+function nothing() {
+    console.log('nothing')
 }
 
 process.stdin.setRawMode(true);
